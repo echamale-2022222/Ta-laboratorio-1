c02f9f6575ba577c927f9322278445526610eea3
echamale-2022222 <echamale-2022222@kinal.edu.gt>
2024-01-24 02:51:43 -0600

    Se modifico el script de index debido a problemas con las animaciones se creo la section pasatiempos asi como tambien se empezo la section lo que mas me gusta se empezo el html y css y js solo falta que funcione

--- assets/scripts/indexScript.js
+++ assets/scripts/indexScript.js
@@ -1,14 +1,28 @@
-var sliderInner = document.querySelector(".slider--inner");
+const slider = document.getElementById('slider');
 
-var images = sliderInner.querySelectorAll("img");
+var contador = 0;
 
-var index = 1;
-
-setInterval(function() {
-    var porcentage = index * -100;
-    sliderInner.style.transform = "translateX(" +  porcentage + "%)";
-    index ++;
-    if (index > (images.length -1)) {
-        index = 0;
+function siguiente() {
+    contador ++;
+    if (contador >= slider.children.length) {
+        contador = 0;
     }
-}, 3000);
+
+    actualizarSlider();
+}
+
+function anterior() {
+    contador --;
+    if (contador < 0) {
+        contador = slider.children.length -1;
+    }
+
+    actualizarSlider();
+}
+
+function actualizarSlider() {
+    const slideWidth = slider.children[0].offsetWidth;
+    slider.style.transform = 'translateX(${-contador * slideWidth}px)';
+}
+
+setInterval(siguiente, 3000);
