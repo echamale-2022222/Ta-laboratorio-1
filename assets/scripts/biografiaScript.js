10ad16269ec0d9883a5227ed985ca9aa805f1ba9
echamale-2022222 <echamale-2022222@kinal.edu.gt>
2024-01-25 07:44:15 -0600

    Pagina biografia terminada y index tambien, solo falta realizar el rediseño de kinal

---
+++ assets/scripts/biografiaScript.js
@@ -0,0 +1,30 @@
+function sobreMi() {
+    var sectionSobremi = document.querySelector('.sobremi');
+
+    sectionSobremi.scrollIntoView({behavior: 'smooth'});
+
+}
+
+function metas() {
+    var sectionMetas = document.querySelector('.metas');
+
+    sectionMetas.scrollIntoView({behavior: 'smooth'});
+}
+
+function habilidades() {
+    var sectionSkill = document.querySelector('.habilidades');
+
+    sectionSkill.scrollIntoView({behavior: 'smooth'});
+}
+
+function multimedia() {
+    var sectionMultimedia = document.querySelector('.multimedia');
+
+    sectionMultimedia.scrollIntoView({behavior: 'smooth'});
+}
+
+function proyectos() {
+    var sectionProyectos = document.querySelector('.proyectos');
+
+    sectionProyectos.scrollIntoView({behavior: 'smooth'});
+}
